d81eca766783927bff0401db087fb74fb7c13f3c
Anirban Bose <158377524+oneerbaan@users.noreply.github.com>
2026-05-26 23:40:10 +0000

    dim reviewed v stocked updated

--- winerevsdata/models/gold/dim_reviewed_vs_stocked.sql
+++ winerevsdata/models/gold/dim_reviewed_vs_stocked.sql
@@ -1,32 +1,70 @@
 WITH wines AS (
+
     SELECT *
     FROM {{ ref('dim_lcbo_wines') }}
+
 ),
 
 reviews AS (
-    SELECT DISTINCT
-        TRIM(LOWER(winery)) AS winery
+
+    SELECT *
     FROM {{ ref('fct_wine_reviews') }}
+
+),
+
+matched_wineries AS (
+
+    SELECT
+
+        reviews.product_name,
+        reviews.country,
+        reviews.region,
+        reviews.points,
+        reviews.variety,
+        reviews.winery,
+
+        wines.producer,
+
+        JAROWINKLER_SIMILARITY(
+            TRIM(LOWER(reviews.winery)),
+            TRIM(LOWER(wines.producer))
+        ) AS similarity_score,
+
+        ROW_NUMBER() OVER (
+            PARTITION BY reviews.winery
+            ORDER BY
+                JAROWINKLER_SIMILARITY(
+                    TRIM(LOWER(reviews.winery)),
+                    TRIM(LOWER(wines.producer))
+                ) DESC
+        ) AS rn
+
+    FROM reviews
+
+    LEFT JOIN wines
+        ON LEFT(TRIM(LOWER(reviews.winery)), 1)
+           =
+           LEFT(TRIM(LOWER(wines.producer)), 1)
+
 )
 
 SELECT
-    wines.product_id,
-    wines.product_name,
+
+    product_name,
+    country,
+    region,
+    points,
+    winery,
+    producer,
+    similarity_score,
 
     CASE
-        WHEN JAROWINKLER_SIMILARITY(TRIM(LOWER(wines.producer)),reviews.winery) >= 88 THEN 'Yes'
+        WHEN similarity_score >= 90 THEN 'Yes'
         ELSE 'No'
     END AS is_available,
 
-    wines.abv,
-    wines.price,
-    wines.producer,
-    wines.product_type,
-    wines.style,
-    wines.country
+    variety
 
-FROM wines
+FROM matched_wineries
 
-LEFT JOIN reviews
-    ON TRIM(LOWER(wines.producer))
-       = reviews.winery
+WHERE rn = 1
